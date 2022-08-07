b3ab9d2650db46b9670e020bf05641d255251ec4
Anna T <annatlovescat@gmail.com>
2022-08-06 21:53:12 -0700

    init

---
+++ backend/backend.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const flightScanner = require('skiplagged-node-api');
+const app = express();
+var cors = require('cors');
+app.use(cors());
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+let prices;
+let brands;
+let resp = [];
+let results_Count = 5;
+let From = 'SEA';
+let To = 'LAX';
+let departure_Date = '2022-10-10';
+
+var searchOptions = {
+  from: From,
+  to: To,
+  departureDate: departure_Date,
+  resultsCount: results_Count,
+  partialTrips: true
+};
+
+function Flight_Scan(){
+flightScanner(searchOptions).then((response) => {
+resp=[]
+brands = response.map(object => object.legs.map(object => object.airline));
+duration = response.map(object => object.duration)
+price = response.map(object => object.price);
+timeofFlight = response.map(object => (`${object.departureTime}=>${object.legs[object.legs.length-1].arrivalTime}`));
+resp.push(brands, price, duration,timeofFlight, searchOptions.resultsCount);
+console.log("Scanned Successfully!", resp)})
+.catch(err => console.log(err))
+}
+app.post('/data',function (req, res) {                                                 
+  searchOptions.resultsCount = Number(req.body.Results_Count);
+  searchOptions.from= String(req.body.From);
+  searchOptions.to= String(req.body.To);
+  searchOptions.departureDate= String(req.body.Departure_date);
+  Flight_Scan()
+  res.send(resp)
+}) 
+app.get('/', function (req, res) {                                                 
+    res.send(resp)
+  });
+app.listen(3000);
---
+++ frontend/index.js
@@ -0,0 +1,36 @@
+const datapage = document.querySelector('#result')
+const form = document.getElementById('form');
+
+fetch('http://localhost:3000/')
+.then(response => response.json())
+.then(data => {for (let i = 0; i < data[data.length-1]; i++)
+{console.log(data);
+datapage.insertAdjacentHTML("beforeend",`<tr>
+<td>${i+1}</td>
+<td>${data[0][i]}</td>
+<td>${data[1][i]}</td>
+<td>${data[2][i]}</td>
+<td>${data[3][i]}</td>
+</tr>`)
+}})
+.catch(err => window.alert("Error Loading your Data"))
+
+form.addEventListener('keypress', (e) =>{
+if(e.key === "Enter"){
+fetch("http://localhost:3000/data",
+{
+    method: "POST",
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(
+        {
+            "To":document.getElementById('box1').value,
+            "From":document.getElementById('box2').value,
+            "Departure_date":document.getElementById('box3').value,
+            "Results_Count":document.getElementById('box4').value
+        }
+    )
+}
+).then(setTimeout(()=>{document.location.reload()},2000))
+}})
